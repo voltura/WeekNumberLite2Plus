bfcd2d8b2ae2c430b28baf8ddc4efd3aed596d9f
Joakim Skoglund <joakim@voltura.se>
2025-06-07 15:10:09 +0200

    minor ico tweaks and refactorings

--- p.cs
+++ p.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
 using System.IO;
@@ -19,79 +18,67 @@ static class P
     private static NotifyIcon icon;
     private static Icon weekIcon;
     private static int currentWeek;
+    private static readonly int[] icoSizes = { 16, 32, 48 };
     private static readonly System.Threading.Mutex Mutex = new System.Threading.Mutex(true, "A2F14B3D-7C9E-489F-8A76-3E7D925F146C");
-    private static readonly string AboutText = string.Format("{0} - v{1}", Application.ProductName, Application.ProductVersion);
     private static readonly string TranslatedWeekText = CultureInfo.CurrentUICulture.LCID == 1053 ? "Vecka" : "Week";
 
     [STAThread]
     static void Main()
     {
-        if (!Mutex.WaitOne(TimeSpan.Zero, true))
-        {
-            return;
-        }
+        if (!Mutex.WaitOne(TimeSpan.Zero, true)) return;
 
         GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
         GCSettings.LatencyMode = GCLatencyMode.Batch;
-
         Application.SetCompatibleTextRenderingDefault(false);
         Application.EnableVisualStyles();
-
         SetProcessDpiAwarenessContext(new IntPtr(-4));
 
-        Timer timer = new Timer() { Interval = 1800000, Enabled = true };
-        timer.Tick += new EventHandler(TimerTick);
+        (new Timer() { Interval = 1800000, Enabled = true }).Tick += UpdateIconTimerTick;
 
-        currentWeek = GetWeek();
-        weekIcon = GetIcon(currentWeek);
         icon = new NotifyIcon()
         {
-            Icon = weekIcon,
+            Icon = weekIcon = GetIcon(currentWeek = GetWeek()),
+            Text = string.Format("{0} {1}\r\n{2:yyyy-MM-dd}", TranslatedWeekText, currentWeek, DateTime.Now),
             Visible = true,
             ContextMenu = new ContextMenu(new MenuItem[]
             {
-                new MenuItem("About", new EventHandler(AboutClick)),
-                new MenuItem("Open web site", new EventHandler(OpenWebsiteClick)),
-                new MenuItem("Save Icon", new EventHandler(SaveIconClick)),
-                new MenuItem("Start with Windows", new EventHandler(StartWithWindowsClick)) { Checked = StartWithWindows },
-                new MenuItem("Exit", new EventHandler(ExitClick))
+                new MenuItem("About", AboutClick),
+                new MenuItem("Open web site", OpenWebsiteClick),
+                new MenuItem("Save Icon", SaveIconClick),
+                new MenuItem("Start with Windows", StartWithWindowsClick) { Checked = StartWithWindows },
+                new MenuItem("Exit", ExitClick)
             })
         };
 
-        UpdateIcon();
-        Application.Run(new ApplicationContext());
+        Application.Run();
     }
 
-    private static void TimerTick(object sender, EventArgs e)
+    private static void UpdateIconTimerTick(object sender, EventArgs e)
     {
         UpdateIcon();
     }
 
     private static void AboutClick(object sender, EventArgs e)
     {
-        MessageBox.Show(AboutText, "About");
+        MessageBox.Show(string.Format("{0} - v{1}", Application.ProductName, Application.ProductVersion), "About");
     }
 
     private static void OpenWebsiteClick(object sender, EventArgs e)
     {
-        Process.Start("https://voltura.github.io/WeekNumberLite2Plus");
+        System.Diagnostics.Process.Start("https://voltura.github.io/WeekNumberLite2Plus");
     }
 
     private static void SaveIconClick(object sender, EventArgs e)
     {
         using (var fs = File.Create(Path.Combine(Environment.GetFolderPath((Environment.SpecialFolder)0x10), "WeekIcon.ico")))
-        {
             weekIcon.Save(fs);
-        }
-
         MessageBox.Show("Icon saved to desktop.", "Saved");
     }
 
     private static void StartWithWindowsClick(object sender, EventArgs e)
     {
         MenuItem menuItem = (MenuItem)sender;
-        StartWithWindows = !menuItem.Checked;
-        menuItem.Checked = StartWithWindows;
+        menuItem.Checked = StartWithWindows = !menuItem.Checked;
     }
 
     private static void ExitClick(object sender, EventArgs e)
@@ -104,48 +91,38 @@ static class P
 
         Mutex.ReleaseMutex();
         Mutex.Dispose();
-
         Application.Exit();
     }
 
     private static int GetWeek()
     {
         DateTime now = DateTime.Now;
-        int year = now.Year;
-        int week = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(now, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
-        int p1 = (year + year / 4 - year / 100 + year / 400) % 7;
-        int p2 = (year - 1 + (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400) % 7;
+        int year = now.Year, week = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(now, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday),
+            p1 = (year + year / 4 - year / 100 + year / 400) % 7, p2 = (year - 1 + (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400) % 7;
 
         return (week == 53 && !(p1 == 4 || p2 == 3)) ? 1 : week;
     }
 
     private static void UpdateIcon()
     {
-        try
+        int week = GetWeek();
+
+        icon.Text = string.Format("{0} {1}\r\n{2:yyyy-MM-dd}", TranslatedWeekText, week, DateTime.Now);
+
+        if (week != currentWeek)
         {
-            int week = GetWeek();
-            icon.Text = string.Format("{0} {1}\r\n{2:yyyy-MM-dd}", TranslatedWeekText, week, DateTime.Now);
+            Icon prevIcon = icon.Icon;
 
-            if (week != currentWeek)
+            icon.Icon = weekIcon = GetIcon(week);
+            currentWeek = week;
+
+            if (prevIcon != null)
             {
-                Icon prevIcon = icon.Icon;
-
-                weekIcon = GetIcon(week);
-                icon.Icon = weekIcon;
-
-                currentWeek = week;
-
-                if (prevIcon != null)
-                {
-                    DestroyIcon(prevIcon.Handle);
-                    prevIcon.Dispose();
-                }
+                DestroyIcon(prevIcon.Handle);
+                prevIcon.Dispose();
             }
         }
-        catch { }
     }
-
-    private static readonly int[] icoSizes = { 16, 32, 48 };
 
     internal static Icon GetIcon(int week)
     {
@@ -170,26 +147,23 @@ static class P
                     using (Graphics g = Graphics.FromImage(bmp))
                     {
                         int inset = Math.Max(1, size / 16);
-                        int fontSize = (size * 25) >> 5;
+                        int fontSize = ((size * 25) >> 5) - 1;
 
                         using (SolidBrush bg = new SolidBrush(Color.Black))
                         using (SolidBrush fg = new SolidBrush(Color.White))
                         using (Pen border = new Pen(Color.LightGray, size == 16 ? 0.1f : 0.2f))
-                        using (Font font = new Font(FontFamily.GenericMonospace, fontSize, (FontStyle)4, GraphicsUnit.Pixel))
+                        using (Font font = new Font(FontFamily.GenericMonospace, fontSize, FontStyle.Bold, GraphicsUnit.Pixel))
                         {
-                            g.FillRectangle(bg, inset, inset, size - inset * 2, size - inset * 2);
-                            Rectangle rect = new Rectangle(inset, inset, size - inset * 2 - 1, size - inset * 2 - 1);
-                            g.DrawRectangle(border, rect);
-
                             int tabWidth = Math.Max(1, size / 10);
                             int tabHeight = Math.Max(1, size / 4) - 2;
                             int tabTop = inset >> 1;
-                            int tabSpacing = Math.Max(2, (size - (2 * tabWidth)) / 5);
+                            int tabSpacing = Math.Max(2, (size - 2 * tabWidth) / 5);
 
+                            g.FillRectangle(bg, inset, inset, size - inset * 2, size - inset * 2);
+                            g.DrawRectangle(border, inset, inset, size - inset * 2 - 1, size - inset * 2 - 1);
                             g.FillRectangle(fg, tabSpacing + 1, tabTop, tabWidth, tabHeight);
                             g.FillRectangle(fg, size - tabSpacing - tabWidth - 1, tabTop, tabWidth, tabHeight);
-
-                            g.DrawString(week.ToString("D2"), font, fg, (size > 16) ? -fontSize * 0.12f : -fontSize * 0.07f, (size > 16) ? fontSize * 0.2f : fontSize * 0.08f);
+                            g.DrawString(week.ToString("D2"), font, fg, (size > 16) ? -fontSize * 0.10f : -fontSize * 0.07f, (size > 16) ? fontSize * 0.16f : fontSize * 0.08f);
                         }
 
                         bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
@@ -211,11 +185,7 @@ static class P
                 imageOffset += images[i].Length;
             }
 
-            for (int i = 0; i < images.Length; i++)
-            {
-                writer.Write(images[i]);
-            }
-
+            for (int i = 0; i < images.Length; i++) writer.Write(images[i]);
             iconStream.Position = 0;
 
             return new Icon(iconStream);
@@ -230,26 +200,15 @@ static class P
             {
                 return Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run", Application.ProductName, null) != null;
             }
-            catch
-            {
-                return false;
-            }
+            catch { return false; }
         }
         set
         {
             try
             {
                 using (var key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true))
-                {
-                    if (value)
-                    {
-                        key.SetValue(Application.ProductName, "\"" + Application.ExecutablePath + "\"");
-                    }
-                    else
-                    {
-                        key.DeleteValue(Application.ProductName, false);
-                    }
-                }
+                    if (value) key.SetValue(Application.ProductName, "\"" + Application.ExecutablePath + "\"");
+                    else key.DeleteValue(Application.ProductName, false);
             }
             catch { }
         }
